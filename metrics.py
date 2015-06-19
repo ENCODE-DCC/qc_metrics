46f0aef673767beac0a76b5f763136e615500c00
nikhilrp <nikhilrp@stanford.edu>
2015-06-19 00:23:10 -0700

    more changes to the code

--- metrics.py
+++ metrics.py
@@ -8,31 +8,21 @@ HEADERS = {'content-type': 'application/json'}
 _PARAMS = {
     'format': ['json'],
     'limit': ['all'],
-    'field': ['files.accession', 'accession', 'assay_term_name']
+    'field': ['original_files']
 }
 
 
-def get_assay_JSON(url):
+def get_file_metadata(href, encode_server, authid, authpw):
+    path = '%s%s' % (encode_server, href)
+    response = requests.get(path, auth=(authid, authpw))
+    return response.json()
+
+
+def get_assay_JSON(url, authid, authpw):
     path = '%s&%s' % (url, urlencode(_PARAMS, True))
-    json_data = open('auth.json')
-    data = json.load(json_data)
-    response = requests.get(path, auth=(data['encoded']['AUTHID'],
-                                        data['encoded']['AUTHPW']))
+    response = requests.get(path, auth=(authid, authpw))
     for exp in response.json()['@graph']:
         yield exp
-
-
-def set_dx_properties():
-    """
-    Loads DX properties from auth.json file and sets the session
-    """
-    json_data = open('auth.json')
-    data = json.load(json_data)
-    try:
-        dxpy.set_workspace_id(data['dnanexus']['project'])
-    except:
-        return False
-    return True
 
 
 def main():
@@ -45,17 +35,31 @@ def main():
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--encode-url', help="ENCODE assay search URL")
     group.add_argument('--dx-file', help="Text file with DNA NEXUS IDs")
-    parser.add_argument('--post-files', help="Post files to encoded",
+    parser.add_argument('--post-metrics', help="Post metrics to encoded",
                         action='store_true', default=False)
 
     args = parser.parse_args()
-    if not set_dx_properties:
-        print "Please set valid DNANEXUS project in the auth.json"
-        raise
 
+    # Load details from JSON doc
+    json_data = open('auth.json')
+    data = json.load(json_data)
+    authid = data['encoded']['authid']
+    authpw = data['encoded']['authpw']
+
+    dxproject = dxpy.DXProject(data['dnanexus']['project'])
     if args.encode_url:
-        for exp in get_assay_JSON(args.encode_url):
-            pass
+        data_dir = data['dnanexus']['data_dir']
+        folders = dxpy.api.project_list_folder(
+            dxproject.id,
+            input_params={'folder': data_dir}
+        )['folders']
+        folders = [folder[(len(data_dir) + 1):] for folder in folders]
+        for exp in get_assay_JSON(args.encode_url, data['encoded']['server'],
+                                  authid, authpw):
+            if exp['accession'] not in folders:
+                continue
+            for f in exp['original_files']:
+                f_json = get_file_metadata(f, authid, authpw)
     elif args.exp_file:
         pass
 
