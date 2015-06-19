7ae4ec74b3f768b8117b99954abef4ed584d6939
nikhilrp <nikhilrp@stanford.edu>
2015-06-19 15:01:08 -0700

    Included more functionality.

--- metrics.py
+++ metrics.py
@@ -2,25 +2,90 @@ import dxpy
 import json
 import requests
 from urllib import urlencode
+from dxpy.exceptions import DXError
+from dxpy.bindings import (
+    dxjob,
+    dxfile,
+    dxanalysis
+)
 
 EPILOG = __doc__
 HEADERS = {'content-type': 'application/json'}
 _PARAMS = {
     'format': ['json'],
     'limit': ['all'],
-    'field': ['original_files']
+    'field': ['accession', 'original_files']
 }
 
 
-def get_file_metadata(href, encode_server, authid, authpw):
-    path = '%s%s' % (encode_server, href)
-    response = requests.get(path, auth=(authid, authpw))
+def patch_file(props):
+    """
+    Patches the file with QC metrics and analysis step run details
+    """
+    pass
+
+
+def check_step_run(alias):
+    """
+    Checks if the step run already exists on the server
+    if yes - return step_run '@id'
+    else - None
+    """
+    params = {
+        'field': ['@id'],
+        'format': ['json'],
+        'aliases': ['dnanexus:%s' % alias],
+        'type': ['analysis_step_run']
+    }
+    path = '%s/search/?%s' % (ENCODE_SERVER, urlencode(params, True))
+    response = requests.get(path, auth=(AUTHID, AUTHPW), verify=False)
+    if len(response.json()['@graph']):
+        return response.json()['@graph'][0]['@id']
+    else:
+        return None
+
+
+def post_step_run(props):
+    """
+    Post the step run to the specificed encode server
+    """
+    pass
+
+
+def load_dx_metadata(props, pipeline):
+    """
+    Takes file properties and parses out dx metadata from notes field
+    returns updated file props with analysis step run and pipeline
+    """
+    notes_json = json.loads(props['notes'])
+    if 'dx-createdBy' not in notes_json:
+        return ()
+    job = dxjob.DXJob(notes_json['dx-createdBy']['job'])
+    file = dxfile.DXFile(notes_json['dx-id'])
+    props['aliases'].append('dnanexus:%s' % file.id)
+    step_run = check_step_run(job.id)
+    if step_run is None:
+        # if there is no step run post it
+        step_run = post_step_run()
+    props['step_run'] = step_run
+    patch_file(props)
+
+
+def get_encode_object(href):
+    """
+    Retrieves object from specified ENCODE server
+    """
+    path = '%s%s' % (ENCODE_SERVER, href)
+    response = requests.get(path, auth=(AUTHID, AUTHPW), verify=False)
     return response.json()
 
 
-def get_assay_JSON(url, authid, authpw):
+def get_assay_JSON(url):
+    """
+    Searches and returns one experiment at a time
+    """
     path = '%s&%s' % (url, urlencode(_PARAMS, True))
-    response = requests.get(path, auth=(authid, authpw))
+    response = requests.get(path, auth=(AUTHID, AUTHPW), verify=False)
     for exp in response.json()['@graph']:
         yield exp
 
@@ -41,27 +106,40 @@ def main():
     args = parser.parse_args()
 
     # Load details from JSON doc
-    json_data = open('auth.json')
-    data = json.load(json_data)
-    authid = data['encoded']['authid']
-    authpw = data['encoded']['authpw']
+    # TODO: should eliminate globals, terrible programming.
+    data = json.load(open('auth.json'))
+    global AUTHID, AUTHPW, ENCODE_SERVER
+    AUTHID = data['encoded']['authid']
+    AUTHPW = data['encoded']['authpw']
+    ENCODE_SERVER = data['encoded']['server']
 
-    dxproject = dxpy.DXProject(data['dnanexus']['project'])
-    if args.encode_url:
-        data_dir = data['dnanexus']['data_dir']
-        folders = dxpy.api.project_list_folder(
-            dxproject.id,
-            input_params={'folder': data_dir}
-        )['folders']
-        folders = [folder[(len(data_dir) + 1):] for folder in folders]
-        for exp in get_assay_JSON(args.encode_url, data['encoded']['server'],
-                                  authid, authpw):
-            if exp['accession'] not in folders:
-                continue
-            for f in exp['original_files']:
-                f_json = get_file_metadata(f, authid, authpw)
-    elif args.exp_file:
-        pass
+    # disable warning for requests should avoid them in first place
+    requests.packages.urllib3.disable_warnings()
+
+    # Check and load pipeline object from the server
+    pipeline = get_encode_object(data['encoded']['pipeline'])
+
+    try:
+        dxproject = dxpy.DXProject(data['dnanexus']['project'])
+    except DXError:
+        print "Please enter a valid project ID in auth.json"
+    else:
+        if args.encode_url:
+            data_dir = data['dnanexus']['data_dir']
+            folders = dxpy.api.project_list_folder(
+                dxproject.id,
+                input_params={'folder': data_dir}
+            )['folders']
+            folders = [folder[(len(data_dir) + 1):] for folder in folders]
+            for exp in get_assay_JSON(args.encode_url):
+                if exp['accession'] not in folders:
+                    continue
+                for f in exp['original_files']:
+                    f_json = get_encode_object(f)
+                    if 'notes' in f_json:
+                        load_dx_metadata(f_json, pipeline)
+        elif args.dx_file:
+            pass
 
 
 if __name__ == '__main__':
