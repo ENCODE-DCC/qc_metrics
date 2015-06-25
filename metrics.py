4aa07d416435e57e9c8e8facc51ea7837643bbb4
nikhilrp <nikhilrp@stanford.edu>
2015-06-25 00:22:29 -0700

    Changes to parse out qc metrics to DNA me pipeline

--- metrics.py
+++ metrics.py
@@ -2,6 +2,7 @@ import dxpy
 import json
 import requests
 import time
+import sys
 from urllib import urlencode
 from dxpy.exceptions import DXError
 from dxpy.bindings import (
@@ -24,148 +25,212 @@ def post_encode_object(collection, props):
         headers=HEADERS)
     if response.status_code == 201:
         return response.json()['@graph'][0]['@id']
+    elif response.status_code == 422:
+        # This should be handled better
+        return id
     else:
         return None
 
 
-def patch_file(props):
+def patch_encode_object(collection, props, id):
     """
     Patches the file with QC metrics and analysis step run details
     """
-    pass
-
-
-def check_workflow_run(analysis):
-    """
-    Checks if the workflow run already exists on the server
-    if yes - return workflow_run '@id'
-    else - None
-    """
-    params = {
-        'field': ['@id'],
-        'format': ['json'],
-        'dx_analysis_id': [analysis],
-        'type': ['workflow_run']
-    }
-    path = '%s/search/?%s' % (data['encode_server'], urlencode(params, True))
-    response = requests.get(path, auth=(
-        data['encode_authid'], data['encode_authpw']))
-    if len(response.json()['@graph']):
+    path = '%s/%s/' % (data['encode_server'], id)
+    response = requests.patch(path, auth=(
+        data['encode_authid'], data['encode_authpw']),
+        data=json.dumps(props),
+        headers=HEADERS)
+    if response.status_code == 200:
         return response.json()['@graph'][0]['@id']
+    elif response.status_code == 422:
+        # This should be handled better
+        return id
     else:
         return None
 
 
-def post_workflow_run(analysis, pipeline):
-    workflow_run = {
-        'dx_analysis_id': analysis.id,
-        'aliases': ['dnanexus:%s' % analysis.id],
-        'pipeline': pipeline.get('@id', None),
-        'started_running': time.strftime(
-            '%Y-%m-%dT%H:%M:%SZ',
-            time.gmtime(analysis.created / 1000.0)
-        ),
-        'stopped_running': time.strftime(
-            '%Y-%m-%dT%H:%M:%SZ',
-            time.gmtime(analysis.modified / 1000.0)
-        ),
-        'status': 'finished' if analysis.state == 'done' else analysis.state
-    }
-    workflow_run = post_encode_object('workflow_run', workflow_run)
-    if workflow_run is None:
-        return None
+def post_workflow_run(analysis):
+    path = '%s/dnanexus:%s' % (data['encode_server'], analysis.id)
+    response = requests.get(path, auth=(data['encode_authid'], data['encode_authpw']))
+    if response.status_code == 200:
+        return response.json()['@id']
     else:
+        workflow_run = {
+            'dx_analysis_id': analysis.id,
+            'aliases': ['dnanexus:%s' % analysis.id],
+            'pipeline': data.get('pipeline'),
+            'started_running': time.strftime(
+                '%Y-%m-%dT%H:%M:%SZ',
+                time.gmtime(analysis.created / 1000.0)
+            ),
+            'stopped_running': time.strftime(
+                '%Y-%m-%dT%H:%M:%SZ',
+                time.gmtime(analysis.modified / 1000.0)
+            ),
+            'status': 'finished' if analysis.state == 'done' else analysis.state
+        }
+        workflow_run = post_encode_object('workflow_run', workflow_run)
         return workflow_run
-
-
-def check_step_run(alias):
-    """
-    Checks if the step run already exists on the server
-    if yes - return analysis_step_run '@id'
-    else - None
-    """
-    params = {
-        'field': ['@id'],
-        'format': ['json'],
-        'aliases': ['dnanexus:%s' % alias],
-        'type': ['analysis_step_run']
-    }
-    path = '%s/search/?%s' % (data['encode_server'], urlencode(params, True))
-    response = requests.get(path, auth=(data['encode_authid'],
-                                        data['encode_authpw']))
-    if len(response.json()['@graph']):
-        return response.json()['@graph'][0]['@id']
-    else:
-        return None
 
 
 def post_step_run(job, workflow_run, analysis_step):
     """
     Post the step run to the specificed encode server
     """
-    applet = dxapplet.DXApplet(job.applet)
-    step_run = {
-        'analysis_step': analysis_step,
-        'workflow_run': workflow_run,
-        'dx_applet_details': {
-            'dx_job_id': job.id,
-            'dx_app_json': applet.describe(),
-            'started_running': time.strftime(
-                '%Y-%m-%dT%H:%M:%SZ',
-                time.gmtime(job.startedRunning / 1000.0)
-            ),
-            'stopped_running': time.strftime(
-                '%Y-%m-%dT%H:%M:%SZ',
-                time.gmtime(job.stoppedRunning / 1000.0)
-            ),
-            'dx_status': 'finished' if applet.state == 'done' else applet.state
-        },
-        'status': 'finished' if job.state == 'done' else job.state
-    }
-    step_run_id = post_encode_object('analysis_step_run', step_run)
-    if workflow_run is None:
-        return None
+    path = '%s/dnanexus:%s' % (data['encode_server'], job.id)
+    response = requests.get(path, auth=(data['encode_authid'], data['encode_authpw']))
+    if response.status_code == 200:
+        return response.json()['@id']
     else:
+        applet = dxapplet.DXApplet(job.applet)
+        step_run = {
+            'analysis_step': analysis_step,
+            'workflow_run': workflow_run,
+            'aliases': ['dnanexus:%s' % job.id],
+            'dx_applet_details': [{
+                'dx_job_id': job.id,
+                'dx_app_json': applet.describe(),
+                'started_running': time.strftime(
+                    '%Y-%m-%dT%H:%M:%SZ',
+                    time.gmtime(job.startedRunning / 1000.0)
+                ),
+                'stopped_running': time.strftime(
+                    '%Y-%m-%dT%H:%M:%SZ',
+                    time.gmtime(job.stoppedRunning / 1000.0)
+                ),
+                'dx_status': 'finished' if applet.state == 'closed' else applet.state
+            }],
+            'status': 'finished' if job.state == 'done' else job.state
+        }
+        step_run_id = post_encode_object('analysis_step_run', step_run)
         return step_run_id
 
 
-def load_dx_metadata(props, pipeline):
+def get_analysis_step(props):
+    """
+    This method is a dirty hack to make the DNA ME pipeline work
+    Terrible programming but can't help it
+    """
+    if props['file_format'] == "bigBed":
+        analysis_step = "/analysis-steps/bigbed-conversion-v-2-6/"
+    elif props['file_format'] == "bam":
+        analysis_step = "/analysis-steps/mott-trim-align-bismark-v-1-0/"
+    elif props['file_format'] == "bed":
+        analysis_step = "/analysis-steps/methylation-quantification-bismark-v-1-0/"
+    return analysis_step
+
+
+def post_qc_metrics(ana_step, step_run, exp_details, folder):
+    """
+    Should load metrics from DNA Nexus stage to file
+    TODO: Will have to extend this to other assays
+    """
+    metrics = {
+        'step_run': step_run,
+        'assay_term_name': exp_details['assay_term_name'],
+        'assay_term_id': exp_details['assay_term_id'],
+        'aliases': ['dnanexus:qc-%s' % step_run]
+    }
+    schema = data['analysis_steps'][ana_step]['metrics']['encode_schema']
+    path = '%s/profiles/%s?format=json' % (data['encode_server'], schema)
+    response = requests.get(path, headers=HEADERS)
+    files = list(dxpy.bindings.search.find_data_objects(
+        folder=folder,
+        name=data['analysis_steps'][ana_step]['metrics']['file_extensions'],
+        name_mode="glob"))
+    file_contents = []
+    lambda_file_contents = []
+    for file in files:
+        file_json = dxfile.DXFile(file.get('id'))
+        if file_json.folder.endswith('lambda'):
+            lambda_file_contents = file_json.read().split('\n')
+        else:
+            file_contents = file_json.read().split('\n')
+
+    # Debug statements to know which files are posted and which aren't
+    if len(file_contents) == 0:
+        print "Couldn't load map report for the file - %s" % folder
+    if len(lambda_file_contents) == 0:
+        print "Couldn't load map report for the file - %s" % folder
+
+    for prop in response.json()['properties']:
+        if prop not in ['@id', '@type', 'status', 'step_run', 'schema_version',
+                        'assay_term_name', 'assay_term_id', 'applies_to',
+                        'status']:
+            # once again hack for DNA me pipeline
+            if prop.startswith('lambda'):
+                for line in lambda_file_contents:
+                    if line.startswith(prop[7:]):
+                        if response.json()['properties'][prop]['type'] == "number":
+                            metrics[prop] = int(line.split('\t')[1])
+                        else:
+                            metrics[prop] = line.split('\t')[1]
+            else:
+                for line in file_contents:
+                    if line.startswith(prop):
+                        if response.json()['properties'][prop]['type'] == "number":
+                            metrics[prop] = int(line.split('\t')[1])
+                        else:
+                            metrics[prop] = line.split('\t')[1]
+    path = '%s/dnanexus:qc-%s' % (data['encode_server'], step_run)
+    response = requests.get(path, auth=(data['encode_authid'], data['encode_authpw']))
+    if response.status_code == 200:
+        patch_encode_object('bismark_qc_metric', metrics)
+    else:
+        post_encode_object('bismark_qc_metric', metrics)
+
+
+def load_metadata(props, exp_details):
     """
     Takes file properties and parses out dx metadata from notes field
     returns updated file props with analysis step run and pipeline
     """
     notes_json = json.loads(props['notes'])
     if 'dx-createdBy' not in notes_json:
-        return
+        print "notes field doesn't have DNA nexus metadata"
+        return False
     job = dxjob.DXJob(notes_json['dx-createdBy']['job'])
     file = dxfile.DXFile(notes_json['dx-id'])
     analysis = dxanalysis.DXAnalysis(job.analysis)
     props['aliases'].append('dnanexus:%s' % file.id)
 
-    # if there is no workflow_run post it
-    workflow_run = check_workflow_run(analysis.id)
-    if workflow_run is None:
-        workflow_run = post_workflow_run(analysis, pipeline)
+    if analysis is None:
+        print "Job doesn't have analysis"
+        return False
 
-    for stage in analysis.stages:
-        if stage.get('id') == job.stage:
-            stage_name = stage['execution']['name']
-            break
+    workflow_run = post_workflow_run(analysis)
 
-    # if there is no step run post it
-    step_run = check_step_run(job.id)
-    if step_run is None:
+    if data['dx_project'] == "project-BKf7zV80z53QbqKQz18005vZ":
+        analysis_step = get_analysis_step(props)
+    else:
+        # grabbing the name of the stage
+        for stage in analysis.stages:
+            if stage.get('id') == job.stage:
+                stage_name = stage['execution']['name']
+                break
+        # selecting the analysis step
         for stage in data['analysis_steps']:
             if data['analysis_steps'][stage]['dx_stage_name'] == stage_name:
                 analysis_step = stage
-        #Manual hack for methaltion pipeline
-        if props['file_format'] == "bigBed" and data['dx_project'] == \
-                "project-BKf7zV80z53QbqKQz18005vZ":
-            analysis_step = "/analysis-steps/bigbed-conversion-v-2-6/"
-
     step_run = post_step_run(job, workflow_run, analysis_step)
-    props['step_run'] = step_run
-    patch_file(props)
+    if step_run is None:
+        return False
+
+    if 'metrics' in data['analysis_steps'][analysis_step]:
+        post_qc_metrics(analysis_step, step_run, exp_details, job.folder)
+    if 'step_run' in props and props['step_run'] == step_run:
+        return True
+    else:
+        patch_dict = {
+            'step_run': step_run,
+            'aliases': props['aliases']
+        }
+        status = patch_encode_object('file', patch_dict, props['@id'])
+        if status is None:
+            return False
+        else:
+            return True
 
 
 def get_encode_object(href):
@@ -188,7 +253,8 @@ def get_assay_JSON(url):
     params = {
         'format': ['json'],
         'limit': ['all'],
-        'field': ['accession', 'original_files']
+        'field': ['accession', 'original_files',
+                  'assay_term_name', 'assay_term_id']
     }
     path = '%s&%s' % (url, urlencode(params, True))
     response = requests.get(path, auth=(data['encode_authid'],
@@ -200,18 +266,15 @@ def get_assay_JSON(url):
 def main():
     import argparse
     parser = argparse.ArgumentParser(
-        description="Module to grab QC metrics from ENCODE uniform processing pipelines",
+        description="Module to grab QC metrics from ENCODE pipelines",
         epilog=EPILOG,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--encode-url', help="ENCODE assay search URL")
     group.add_argument('--dx-file', help="Text file with DNA NEXUS IDs")
-    parser.add_argument('--post-metrics', help="Post metrics to encoded",
-                        action='store_true', default=False)
 
     args = parser.parse_args()
-    pipeline = get_encode_object(data['pipeline'])
 
     try:
         dxpy.set_workspace_id(data['dx_project'])
@@ -219,12 +282,23 @@ def main():
         print "Please enter a valid project ID in auth.json"
     else:
         if args.encode_url:
+            if 'assay_term_name' not in args.encode_url:
+                print "Please select exactly one assay type."
+                sys.exit(1)
             for exp in get_assay_JSON(args.encode_url):
+                exp_details = {
+                    'assay_term_name': exp['assay_term_name'],
+                    'assay_term_id': exp['assay_term_id'],
+                    'accession': exp['accession']
+                }
+                print "Started processing the experimet - %s" % exp['accession']
                 for f in exp['original_files']:
                     f_json = get_encode_object(f)
                     if 'notes' in f_json:
-                        load_dx_metadata(f_json, pipeline)
-                print "Done - " + exp['accession']
+                        status = load_metadata(f_json, exp_details)
+                        if not status:
+                            print "Couldn't update the - %s" % f_json['accession']
+                print "     Finished processing the experiment - %s" % exp['accession']
         elif args.dx_file:
             pass
 
